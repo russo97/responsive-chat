d730edf3a9c48597243d9578ccd789c6022a5b75
Wanderson Silva <wandersonsilva768@gmail.com>
2023-01-25 23:52:54 -0300

    feat: adição do SCSS e criação do componente ChatHeader

---
+++ src/components/ChatHeader.tsx
@@ -0,0 +1,10 @@
+
+export function ChatHeader () {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <img src="https://randomuser.me/api/portraits/women/3.jpg" alt=""/>
+      </div>
+    </header>
+  )
+}
